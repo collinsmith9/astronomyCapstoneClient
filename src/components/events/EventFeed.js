d62e0532b2eb7a05d979c33f648a7409ce59cb88
collin <geniuscollin@gmail.com>
2022-03-22 14:16:58 -0500

    removed some comments on accident, might as well push

--- src/components/events/EventFeed.js
+++ src/components/events/EventFeed.js
@@ -29,13 +29,6 @@ function EventFeed() {
     function syncUsersEventLikes() {
         getUsersEventLikes(user).then(setUsersEventLikes)
     }
-
-    // const didUserLike = (event) => {usersEventLikes.find((eventLike) => {
-    //     if (+eventLike.event?.id === +event.id) {
-    //         return true
-    //     }
-    //     return false
-    // })} 
     
     function handleEventLike(event) {
         
@@ -109,44 +102,3 @@ function EventFeed() {
 }
 
 export default EventFeed;
-
-// import React, { useEffect, useState } from "react";
-// import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
-// import { getPosts } from "./postManager";
-
-// function MainFeed() {
-//     const [posts, setPosts] = useState([])
-//     const user = +localStorage.getItem("astronomer")
-//     const history = useHistory()
-
-//     useEffect(() => {
-//         getPosts().then(setPosts)
-//     },[])
-    
-
-
-//     return (
-//         <>
-//         <h2>Main feed</h2>
-
-//         <button onClick={() => {history.push("/newpost")}}>New Post</button>
-
-//         {
-//             posts.map((post) => {
-//                 return <fieldset key={post.id}>
-//                     <div>
-//                         <p>Posted by: {post.user?.user?.first_name} {post.user?.user?.last_name}</p>
-//                         <h4>Caption: </h4><p>{post.caption}</p>
-//                         <div><img src={`http://localhost:8000${post.post_pic}`} alt="hello" /></div>
-//                     </div>
-//                 </fieldset>
-//             })
-//         }
-
-//         </>
-//     )
-
-
-// }
-
-// export default MainFeed
